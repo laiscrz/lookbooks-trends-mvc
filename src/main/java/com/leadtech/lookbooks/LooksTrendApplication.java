b11f590b3b04a5a629e3032b5fa48f981da274f4
laiscrz <lais.albino2003@gmail.com>
2024-10-31 19:42:56 -0300

    feat: initialize project

---
+++ src/main/java/com/leadtech/lookbooks/LooksTrendApplication.java
@@ -0,0 +1,13 @@
+package com.leadtech.lookbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LooksTrendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LooksTrendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leadtech/lookbooks/LooksTrendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leadtech.lookbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LooksTrendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
