13a6ec440aaa425959ce1cba6120a977ca49286b
laiscrz <lais.albino2003@gmail.com>
2024-11-02 14:17:40 -0300

    feat: created method type delete lookbook

--- src/main/java/com/leadtech/lookbooks/controller/LookbookController.java
+++ src/main/java/com/leadtech/lookbooks/controller/LookbookController.java
@@ -1,11 +1,15 @@
 package com.leadtech.lookbooks.controller;
 
 import com.leadtech.lookbooks.model.Lookbook;
+import com.leadtech.lookbooks.model.enums.StyleType;
+import com.leadtech.lookbooks.model.enums.TrendType;
 import com.leadtech.lookbooks.service.LookbookService;
 import com.leadtech.lookbooks.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -27,5 +31,36 @@ public class LookbookController {
         ModelAndView modelAndView = new ModelAndView("lookbook/list");
         modelAndView.addObject("lookbooks", lookbooks);
         return modelAndView;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView showLookbookDetails(@PathVariable Long id) {
+        Lookbook lookbook = lookbookService.findByIdLookbook(id);
+        ModelAndView modelAndView = new ModelAndView("lookbook/details");
+        modelAndView.addObject("lookbook", lookbook);
+        return modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView deleteLookbook(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("lookbook/list");
+        try {
+            lookbookService.deleteByIdILookbook(id);
+            modelAndView.addObject("message", "Lookbook excluído com sucesso.");
+        } catch (RuntimeException e) {
+            modelAndView.addObject("error", e.getMessage());
+        }
+        modelAndView.addObject("lookbooks", lookbookService.findAllLookbooks());
+        return modelAndView;
+    }
+
+    @ModelAttribute("estilo")
+    public StyleType[] getEstilo(){
+        return StyleType.values();
+    }
+
+    @ModelAttribute("tendencia")
+    public TrendType[] getTendencia(){
+        return TrendType.values();
     }
 }
