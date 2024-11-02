262b44d4da054bbb562fee1a09ddaae63f8f156e
laiscrz <lais.albino2003@gmail.com>
2024-11-02 10:07:24 -0300

    feat: updated page list

---
+++ src/main/java/com/leadtech/lookbooks/controller/LookbookController.java
@@ -0,0 +1,9 @@
+package com.leadtech.lookbooks.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/lookbooks")
+public class LookbookController {
+}
