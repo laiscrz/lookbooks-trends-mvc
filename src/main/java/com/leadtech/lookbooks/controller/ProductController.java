5c5330054b113272e5ba093c83def0dd455f3933
laiscrz <lais.albino2003@gmail.com>
2024-11-02 12:06:02 -0300

    feat: created method type delete product

--- src/main/java/com/leadtech/lookbooks/controller/ProductController.java
+++ src/main/java/com/leadtech/lookbooks/controller/ProductController.java
@@ -1,10 +1,16 @@
 package com.leadtech.lookbooks.controller;
 
 import com.leadtech.lookbooks.model.Product;
+import com.leadtech.lookbooks.model.enums.CategoryType;
+import com.leadtech.lookbooks.model.enums.DesignType;
+import com.leadtech.lookbooks.model.enums.SeasonalityType;
+import com.leadtech.lookbooks.model.enums.SizeType;
 import com.leadtech.lookbooks.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,5 +29,49 @@ public class ProductController {
         ModelAndView modelAndView = new ModelAndView("product/list");
         modelAndView.addObject("products", products);
         return modelAndView;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView showProductDetails(@PathVariable Long id) {
+        Product product = productService.findByIdProduct(id);
+        ModelAndView modelAndView = new ModelAndView("product/details");
+        modelAndView.addObject("product", product);
+        return modelAndView;
+    }
+
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView deleteProduct(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("product/list");
+        try {
+            productService.deleteByIdProduct(id);
+
+            modelAndView.addObject("message", "Produto excluído com sucesso.");
+        } catch (RuntimeException e) {
+            modelAndView.addObject("error", e.getMessage());
+        }
+        modelAndView.addObject("products", productService.findAllProducts());
+        return modelAndView;
+    }
+
+    @ModelAttribute("categoria")
+    public CategoryType[] getCategoria(){
+        return CategoryType.values();
+    }
+
+    @ModelAttribute("tamanho")
+    public SizeType[] getTamanho(){
+        return SizeType.values();
+    }
+
+    @ModelAttribute("design")
+    public DesignType[] getDesign(){
+        return DesignType.values();
+    }
+
+
+    @ModelAttribute("sazonalidade")
+    public SeasonalityType[] getSazonalidade(){
+        return SeasonalityType.values();
     }
 }
