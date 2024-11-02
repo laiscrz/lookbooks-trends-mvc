95ab36e7ea3e5345044d8400703682508e1f620e
laiscrz <lais.albino2003@gmail.com>
2024-11-02 10:15:31 -0300

    feat: created page list lookbook

--- src/main/java/com/leadtech/lookbooks/controller/LookbookController.java
+++ src/main/java/com/leadtech/lookbooks/controller/LookbookController.java
@@ -1,9 +1,31 @@
 package com.leadtech.lookbooks.controller;
 
+import com.leadtech.lookbooks.model.Lookbook;
+import com.leadtech.lookbooks.service.LookbookService;
+import com.leadtech.lookbooks.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/lookbooks")
 public class LookbookController {
+
+    @Autowired
+    private LookbookService lookbookService;
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ModelAndView showListLookbooks() {
+        List<Lookbook> lookbooks = lookbookService.findAllLookbooks();
+        ModelAndView modelAndView = new ModelAndView("lookbook/list");
+        modelAndView.addObject("lookbooks", lookbooks);
+        return modelAndView;
+    }
 }
