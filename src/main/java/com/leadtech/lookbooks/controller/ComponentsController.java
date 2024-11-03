b3657e09fb091679e7e50234f7d7bbd6fc893faa
laiscrz <lais.albino2003@gmail.com>
2024-11-03 13:13:33 -0300

    feat: updated class

--- src/main/java/com/leadtech/lookbooks/controller/ComponentsController.java
+++ src/main/java/com/leadtech/lookbooks/controller/ComponentsController.java
@@ -10,9 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
